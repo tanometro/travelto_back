cb6208513f04d610e8b15d43ac5a43700366603d
tanometro <angelogrillo@bitecnologias.com>
2023-10-24 18:37:10 -0300

    Nuevos cambios

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const port = 3001;
-
-const server = require('./src/server');
-const { conn } = require('./src/db')
-require('./src/db');
-
-conn.sync({force: true})
-  .then(() => {
-    server.listen(port, () => {
-      console.log(`Server raised in port: ` + port);
-    });
+const {
+  dataAttraction,
+} = require("./src/controllers/attractions.controllers.js");
+const server = require("./src/server");
+const { conn } = require("./src/db");
+require("./src/db");
+conn
+  .sync({ force: true })
+  .then(async () => {
+    try {
+      await dataAttraction();
+      console.log("Datos cargados en la base de datos.");
+      // Iniciar el servidor después de cargar los datos
+      server.listen(port, () => {
+        console.log(`Servidor iniciado en el puerto: ${port}`);
+      });
+    } catch (error) {
+      console.error("Error al cargar datos en la base de datos:", error);
+    }
   })
-  .catch((error) => console.log(error));
-
-
+  .catch((error) => {
+    console.error("Error al sincronizar la base de datos:", error);
+  });
