c8b067b9d0cfd7c600c57528656c91613df76b2c
tanometro <angelogrillo@bitecnologias.com>
2023-10-25 22:35:51 -0300

    Swagger

--- src/server.js
+++ src/server.js
@@ -4,9 +4,15 @@ const routes = require('./routes/index.js');
 const morgan = require("morgan");
 const cors = require("cors");
 
+// Swagger mi rey
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerSpec()
+
 server.use(express.json());
 server.use(morgan('dev'));
 server.use(cors());
 server.use('/', routes);
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 
 module.exports = server;
