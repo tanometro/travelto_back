5ee8ef0bbae64f7de86e6a52dbb8175a510bc967
ProfeCandy <matias.quintanaballesteros@gmail.com>
2023-10-27 12:49:49 -0300

    FILTRADOS X CIUDAD X COUNTRY X PRECIO HECHOS Y FUNCIONALES

--- src/routes/attractions.routes.js
+++ src/routes/attractions.routes.js
@@ -1,14 +1,21 @@
 const { Router } = require('express')
 const router = Router()
 const { readAllAttraction, readAttractionById,readAttractionByQuery, createNewAttraction, dataAttraction, updateAttraction, deleteAttraction } = require('../controllers/attractions.controllers')
+const { filterAtracByCity, OrderByPrice } = require('../controllers/filter.controller')
 
 router.get('/data', dataAttraction);
 router.get('/name', readAttractionByQuery);
+
+
+router.get('/orderByPrice', OrderByPrice)
 router.get('/:id', readAttractionById);
+
 router.get('/', readAllAttraction);
 router.post('/create', createNewAttraction);
 router.put('/update/:id', updateAttraction);
 router.delete('/delete/:id', deleteAttraction);
+
+router.get('/city/:city', filterAtracByCity)
 
 
 module.exports = router
--- src/routes/locations.routes.js
+++ src/routes/locations.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 const {dataLocal,createLocation, readAllLocations, updateLocation, deleteLocation, readOneLocation, getLocationByQuery} = require('../controllers/locations.controlers');
+const { filterLocByCountry } = require('../controllers/filter.controller')
 
 router.get('/data', dataLocal)
 router.get('/name', getLocationByQuery)
@@ -9,5 +10,7 @@ router.get('/', readAllLocations);
 router.post('/create', createLocation);
 router.put('/update/:id', updateLocation)
 router.delete('/delete/:id', deleteLocation);
+
+router.get('/country/:country', filterLocByCountry)
 
 module.exports = router;
