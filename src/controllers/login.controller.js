76471f8f2c9fdc53a7075554be0b9daac3d01f01
ProfeCandy <matias.quintanaballesteros@gmail.com>
2023-11-02 14:37:57 -0300

    Login reformulado, Y cambios en rutas de user (name - lastName), 100% andando

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,28 +1,25 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const { login } = require('../services/login.services');
 
-const secretKey = 'Dracarys'
-
-const loginFunction = async (req, res)=>{
-  const { email, password } = req.body;
+const loginFunction = async (req, res) => {
   try {
-    const user = await findUser(email);
- 
-     if (!user.dataValues.email) {
-       return res.status(401).send('Credenciales inválidas, no existe usuario');
-     }
+    const { email, password } = req.query;
 
-     const valid = await bcrypt.compare(password, user.password);
-     if (!valid) {
-       return res.status(401).send('Credenciales inválidas, contraseña incorrecta');
-     }
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Faltan datos' });
+    }
 
-     const token = jwt.sign({ email }, secretKey, { expiresIn: '1h' });
-     res.status(200).json({ token });
+    const result = await login(email, password);
+
+    if (result.error) {
+      return res.status(401).json({ message: result.error });
+    }
+
+    res.json(result);
   } catch (error) {
-    res.status(500).json({ error: "Error en el servidor de login"});
+    res.status(500).json({ error: 'Error en el servidor de login' });
   }
-  
-}
+};
 
-module.exports = loginFunction
+module.exports = {
+  loginFunction
+};
--- src/services/login.services.js
+++ src/services/login.services.js
@@ -1,18 +1,38 @@
-const {User} = require('../db');
+const { User } = require('../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const findUser = async (email) => {
-    try {
-      const user = await User.findOne({
-            where: {
-               email: email
-             }
-         });
-    return user
-    } catch (error) {
-        throw new Error(error.message);
+const secretKey = 'Dracarys';
+
+const login = async (email, password) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: email
+      }
+    });
+
+    if (!user) {
+      return { error: 'Credenciales inválidas' };
     }
-}
+
+    if (bcrypt.compareSync(password, user.password)) {
+      let token = jwt.sign({ user: user }, secretKey, {
+        expiresIn: '1h',
+      });
+
+      return {
+        user: user,
+        token: token,
+      };
+    }
+
+    return { error: 'Contraseña incorrecta' };
+  } catch (error) {
+    throw new Error('Error en el servidor de login');
+  }
+};
 
 module.exports = {
-    findUser
-}
+  login
+};
