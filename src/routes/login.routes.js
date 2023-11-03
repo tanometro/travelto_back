2debe962952d4cfacfa50b7753bdb5a8d56577f4
ProfeCandy <matias.quintanaballesteros@gmail.com>
2023-11-03 12:53:04 -0300

    cambie la ruta a post del login

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -2,6 +2,6 @@ const { Router } = require('express')
 const router = Router()
 const {loginFunction}= require('../controllers/login.controller')
 
-router.get('/', loginFunction);
+router.post('/', loginFunction);
 
 module.exports = router;
