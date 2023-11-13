ab94d26874dd651d3cebf46e664f864df59c1b52
Nicolas Mennichelli <74836386+michiqueli@users.noreply.github.com>
2023-11-13 20:19:19 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -28,17 +28,19 @@ const swaggerSpec = {
 server.use(express.urlencoded({ extended: false }))
 server.use(express.json());
 server.use(morgan('dev'));
-server.use(cors());
+
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+server.use(cors());
 server.use('/', routes);
 server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 
