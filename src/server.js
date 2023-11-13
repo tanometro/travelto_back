4d1a8a61dc6ec6c6a6e7cb7b5285fe370bb3cb30
Nicolas Mennichelli <74836386+michiqueli@users.noreply.github.com>
2023-11-13 19:02:26 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -33,17 +33,6 @@ server.use(express.urlencoded({ extended: false }))
 server.use(express.json());
 server.use(morgan('dev'));
 server.use(cors());
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
 server.use('/', routes);
 server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 
