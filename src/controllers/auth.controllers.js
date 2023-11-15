777f891be1b8bc31a23a0a6f2bf2b1eb1368c2e5
ricardo <rblazquez111@gmail.com>
2023-11-15 15:24:49 -0300

    ADD isUser y isOperator

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,28 +1,63 @@
-const {findUser, token} = require('../services/auth.services')
+const { findUser, token } = require('../services/auth.services')
 
 const auth = async (req, res) => {
-    const {email, password} = req.body;
-    try {
-        const response = await findUser(email, password);
-        const user = {email: email}
-        const accessToken = generateAccessToken(user)
-    } catch (error) {
-        
-    }
+  const { email, password } = req.body;
+  try {
+    const response = await findUser(email, password);
+    const user = { email: email }
+    const accessToken = generateAccessToken(user)
+  } catch (error) {
+
+  }
 }
+
 
 //! ---------------------------------------------------------------
 const authenticateUser = async (req, res, next) => {
-    try {
-      const authtoken = req.headers.authorization.split(' ')[1];
-      const user = await token(authtoken);
+  try {
+    const authtoken = req.headers.authorization.split(' ')[1];
+    const user = await token(authtoken);
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+};
+
+const isAdmin = async (req, res, next) => {
+  try {
+    const user = findUser(req.user.email);
+    if (user.roleID === 1) {
       req.user = user;
       next();
-    } catch (error) {
-      return res.status(401).json({ message: error.message });
+    } else {
+      return res.status(401).json({ message: 'Usuario no autorizado' });
     }
-  };
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+}
 
-  module.exports = {
-    authenticateUser
-  };
+const isOperator = async (req, res, next) => {
+  try {
+    const user = findUser(req.user.email);
+    if (user.roleID === 2) {
+      req.user = user;
+      next();
+    } else {
+      return res.status(401).json({ message: 'Usuario no autorizado' });
+    }
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+}
+
+
+
+
+
+module.exports = {
+  authenticateUser,
+  isAdmin,
+  isOperator
+};
