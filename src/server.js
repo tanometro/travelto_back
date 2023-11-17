022154a10667804095c8ba1dc8ab1dc40c24d1df
tanometro <angelogrillo@bitecnologias.com>
2023-11-17 17:19:27 -0300

    MercadoPagamentos

--- src/server.js
+++ src/server.js
@@ -6,23 +6,77 @@ const morgan = require("morgan");
 const mercadopago = require('mercadopago');
 const path = require('path');
 const { BASE_URL, ACCESS_TOKEN } = process.env;
-  
+
+server.use(express.urlencoded({ extended: false }))
+server.use(express.json());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+//server.use(cors());
+server.use('/', routes);
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerSpec = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'TravelTo docs API',
+            version: '1.0.0',  
+        },
+        servers: [
+            {
+                url: `${BASE_URL}`
+            }
+        ]
+    },
+    apis: [`${path.join(__dirname, './routes/*.js')}`]
+};
+
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
+
+
+mercadopago.configure({
+  access_token: ACCESS_TOKEN
+});
+
   server.post("/create_preference", (req, res) => {
+
     let preference = {
       items: [
         {
-          title: req.body.description,
+          id: req.body.id,
+          title: req.body.description, 
           unit_price: Number(req.body.price),
           quantity: Number(req.body.quantity),
+
         },
       ],
+      payer: {
+        name: req.body.payerName,
+        surname: req.body.payerSurname,
+        email: req.body.payerEmail,
+        identification: {
+            type: 'DNI',
+            number: req.body.identification.number,
+        },
       back_urls: {
         success: "http://localhost:3001",
         failure: "http://localhost:3001",
         pending: "",
       },
       auto_return: "approved",
-    };
+    }
+  }
   
     mercadopago.preferences
       .create(preference)
@@ -44,42 +98,5 @@ const { BASE_URL, ACCESS_TOKEN } = process.env;
     });
   });
 
-// Swagger
-const swaggerUI = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerSpec = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'TravelTo docs API',
-            version: '1.0.0',  
-        },
-        servers: [
-            {
-                url: `${BASE_URL}`
-            }
-        ]
-    },
-    apis: [`${path.join(__dirname, './routes/*.js')}`]
-};
-
-server.use(express.urlencoded({ extended: false }))
-server.use(express.json());
-server.use(morgan('dev'));
-
-server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-//server.use(cors());
-server.use('/', routes);
-server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 
 module.exports = server;
